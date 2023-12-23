69053f91101a1845bdb7a51652fd2114c61eee31
arsMM22 <129196828+arsMM22@users.noreply.github.com>
2023-12-23 15:32:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from dotenv import load_dotenv
+import argparse
+import os
+import requests
+from urllib.parse import urlparse
+
+
+def shorten_link(token, link):
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    params = {"long_url": link}
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(url, headers=headers, json=params)
+    response.raise_for_status()
+    return response.json()['id']
+
+
+def count_clicks(token, bitlink):
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    params = {"unit": "month", "units": -1}
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(token, bitlink):
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers)
+    return response.ok
+
+
+def main():
+    load_dotenv()
+    token = os.environ['BITLY_TOKEN']
+    parser = argparse.ArgumentParser(description='���� ������ �������� ��� ��������� ������ � ������� ����� ������')
+    parser.add_argument('--url', type=str, help='������� ������')
+    args = parser.parse_args()
+    parsed_url=urlparse(args.url)
+    parsed_url=f"{parsed_url.netloc}{parsed_url.path}"
+    try:
+        if is_bitlink(token, parsed_url):
+            print("���������� ������: ", count_clicks(token, parsed_url))
+        else:
+            print(shorten_link(token, args.url))
+    except requests.exceptions.HTTPError:
+        print("������, �������� ������.")
+
+
+if __name__ == "__main__":
+    main()
